a6c066a7fe70817d8c82a2777a2e2f7bf9c0a89c
CEOSPEEDBIRD <ceo.uz@gmx.de>
2022-01-17 12:02:08 +0100

    Added Disclosure in Section and Groupbox

---
+++ Sources/FancyUI/DisclosureSection.swift
@@ -0,0 +1,75 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Luca on 17/01/2022.
+//
+
+import SwiftUI
+
+struct DisclosureSection<Content: View>: View {
+    var title: String?
+    var symbolChoice: expandSymbol?
+    var multicolorSymbol: Bool?
+    var showDivider: Bool?
+    
+    @State var showingDetail: Bool = false
+    @ViewBuilder var content: Content
+    
+        
+    var body: some View {
+        if #available(iOS 14.0, *) {
+            Section {
+                HStack {
+                    Text(title ?? "")
+                        .font(.system(.headline, design: .rounded))
+                    Spacer()
+                    Button(action: {
+                        withAnimation {
+                            showingDetail.toggle()
+                        }
+                    }) {
+                        if multicolorSymbol ?? false {
+                            Image(systemName: (symbolChoice ?? .chevronCircle).rawValue)
+                                .renderingMode(.original)
+                                .imageScale(.large)
+                                .rotationEffect(.degrees(showingDetail ? 90 : 0))
+                                .scaleEffect(showingDetail ? 1.25 : 1)
+                                .padding(.trailing, 5)
+                        } else {
+                            Image(systemName: (symbolChoice ?? .chevronCircle).rawValue)
+                                .foregroundColor(.accentColor)
+                                .imageScale(.large)
+                                .rotationEffect(.degrees(showingDetail ? 90 : 0))
+                                .scaleEffect(showingDetail ? 1.25 : 1)
+                                .padding(.trailing, 5)
+                        }
+                    }.buttonStyle(PlainButtonStyle())
+                }.onTapGesture {
+                    withAnimation {
+                        showingDetail.toggle()
+                    }
+                }
+                
+                if showingDetail {
+                    if showDivider ?? false {
+                        Divider()
+                    }
+                    
+                    content
+                        .padding(.vertical, 5)
+                }
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+}
+
+struct DisclosureSection_Previews: PreviewProvider {
+    static var previews: some View {
+        DisclosureSection(title: "Hello") {
+            Text(verbatim: "Hello, world")
+        }
+    }
+}
